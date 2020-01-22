9f997445fac0ff8ef528bc9de43a210c8bfeecb1
Raphael Cuba <raphael@raphaelcuba.com>
2020-01-22 14:33:51 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,5 @@ Rails.application.routes.draw do
     end
   end
 
-  constraints subdomain: 'www' do
-    resources :home, only: :index
-  end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
 end
